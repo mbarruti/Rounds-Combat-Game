6a2709f6e1ec08ef51f6bc7fd13b3faec6f6e358
mbarruti <mbarruti5@gmail.com>
2025-11-26 22:12:54 +0100

    Automatized special weapon actions for their SO

---
+++ Assets/TurnsGame/Scripts/Weapons/GreathammerSO.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WarhammerSO", menuName = "Weapons/Warhammer")]
+public class WarhammerSO : WeaponSO
+{
+    [field: Header("Editor data")]
+    bool defaultSpecialActionsInitialized = false;
+
+    #if UNITY_EDITOR
+    void OnEnable()
+    {
+        if (!Application.isPlaying && !defaultSpecialActionsInitialized)
+        {
+            if (SpecialActions == null) SpecialActions = new();
+
+            ChargeSO chargeSO = ScriptableObjectUtils.GetAsset(
+                                    "Charge", typeof(ChargeSO)) as ChargeSO;
+            TackleSO tackleSO = ScriptableObjectUtils.GetAsset(
+                                    "Tackle", typeof(TackleSO)) as TackleSO;
+
+            if(chargeSO != null && !SpecialActions.Contains(chargeSO))
+                SpecialActions.Add(chargeSO);
+
+            if(tackleSO != null && !SpecialActions.Contains(tackleSO))
+                SpecialActions.Add(tackleSO);
+        }
+
+        defaultSpecialActionsInitialized = true;
+
+        UnityEditor.EditorUtility.SetDirty(this);
+    }
+    #endif
+}
--- Assets/TurnsGame/Scripts/Weapons/WeaponSO.cs
+++ Assets/TurnsGame/Scripts/Weapons/WeaponSO.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/Weapon")]
@@ -7,6 +9,7 @@ public class WeaponSO : ScriptableObject
     [field: Header("General")]
     [field: SerializeField] public string Name { get; private set; } = "None";
     [field: SerializeField] public CharacterActionSO SpecialAction { get; private set; }
+    [field: SerializeField] public List<CharacterActionSO> SpecialActions { get; protected set; }
 
     [field: Header("One-handed")]
     [field: SerializeField] public float BaseDamage { get; private set; } = 20;
@@ -23,4 +26,22 @@ public class WeaponSO : ScriptableObject
     [field: SerializeField] public float DualProwess { get; private set; } = 1;
     [field: SerializeField] public float DualCounterChance { get; private set; } = 0.05f;
     [field: SerializeField] public int DualNumHits { get; private set; } = 1;
+}
+
+public static class ScriptableObjectUtils
+{
+    public static object GetAsset(string SOName, Type type)
+    {
+        string[] matchingAssets = AssetDatabase.FindAssets(
+            SOName, new string[]{"Assets/TurnsGame/Prefabs/Character Actions"});
+
+        foreach (var asset in matchingAssets)
+        {
+            var SOpath = AssetDatabase.GUIDToAssetPath(asset);
+            var SO = AssetDatabase.LoadAssetAtPath(SOpath, type);
+            return SO;
+        }
+
+        return null;
+    }
 }
