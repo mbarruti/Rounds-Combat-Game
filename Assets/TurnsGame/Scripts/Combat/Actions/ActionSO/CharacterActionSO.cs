b472e5bfb6a212b9470bb0643c19a23fe17cf450
mbarruti <mbarruti5@gmail.com>
2025-11-26 20:02:11 +0100

    Added names to Actions SO

--- Assets/TurnsGame/Scripts/Combat/Actions/ActionSO/CharacterActionSO.cs
+++ Assets/TurnsGame/Scripts/Combat/Actions/ActionSO/CharacterActionSO.cs
@@ -4,6 +4,7 @@ using static MyProject.Constants;
 
 public abstract class CharacterActionSO : ScriptableObject
 {
+    [field: SerializeField] public string Name { get; private set; } = "None";
     [field: SerializeField] public ActionPriority Lead { get; private set; } = NONE;
     [field: SerializeField] public bool CanRecoverMeter { get; private set; }
 
